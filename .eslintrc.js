5dc32d92c33a022f47d454796fec44bafff69d21
Lakshmy08 <rajalakshmynv0803@gmail.com>
2025-10-04 16:21:27 +0530

    fix: eslint added

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,22 @@
 module.exports = {
   env: {
-    browser: false, // Set true if you also have frontend code here
-    node: true,     // Enables Node.js global variables
-    es2021: true,   // Enables ES2021 syntax (like optional chaining)
-    jest: true,     // Enables Jest globals (for unit/integration tests)
+     
+    node: true,    
+    es2021: true,  
+    jest: true,    
   },
   extends: [
-    "eslint:recommended",          // Basic recommended rules
-    "plugin:prettier/recommended", // Integrate Prettier (auto formatting)
-  ],
+    "eslint:recommended"],
   parserOptions: {
-    ecmaVersion: "latest", // Allows newest JS syntax
-    sourceType: "module",  // Enable import/export
+    ecmaVersion: 12,
+    sourceType: "module",  
   },
   rules: {
    
-    "no-console": "off",           // Allow console.log (useful for backend)
-    "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-    "prefer-const": "warn",
-    "eqeqeq": ["error", "always"],
-    "curly": ["error", "all"],
-    "semi": ["error", "always"],
+    "no-console": 'warn',          
+    "no-unused-vars": 'error',
+    "prefer-const": 'error'
+   
   },
 };
  
